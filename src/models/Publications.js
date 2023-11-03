a1e9c96a366d5556240037195762c9b78e3d1bdf
Ignacio Cunial <igsecu@hotmail.com>
2023-11-03 15:32:06 -0300

    Created Publication database model

---
+++ src/models/Publications.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require("sequelize");
+const db = require("../db");
+
+const Publication = db.define(
+  "publication",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    isBanned: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    isImportant: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      defaultValue: null,
+    },
+    image_id: {
+      type: DataTypes.STRING,
+      defaultValue: false,
+    },
+  },
+  {
+    timestamps: true,
+    createdAt: true,
+    updatedAt: false,
+  }
+);
+
+module.exports = Publication;
