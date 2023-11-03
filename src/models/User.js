b2266cd8d7714c9792222a9083bd6b9647bfb96a
Ignacio Cunial <igsecu@hotmail.com>
2023-11-03 14:15:34 -0300

    Created User database model

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize");
+const db = require("../db");
+
+const User = db.define(
+  "user",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+    createdAt: true,
+    updatedAt: false,
+  }
+);
+
+module.exports = User;
