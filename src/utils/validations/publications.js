1744f75e120cd950591cb943246fca3a51287415
Ignacio Cunial <igsecu@hotmail.com>
2023-11-06 15:55:56 -0300

    Created validateDescription() function

--- src/utils/validations/publications.js
+++ src/utils/validations/publications.js
@@ -4,3 +4,15 @@ const validateTitle = (title) => {
   if (typeof title !== "string") return "Title must be a string";
   return false;
 };
+
+// Validate description
+const validateDescription = (description) => {
+  if (!description) return "Description parameter is missing";
+  if (typeof description !== "string") return "Description must be a string";
+  return false;
+};
+
+module.exports = {
+  validateTitle,
+  validateDescription,
+};
